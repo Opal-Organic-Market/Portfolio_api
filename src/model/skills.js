01ba35f28c8383cfeb2bc7b572ef963c28508579
teyvi <44750503+teyvi@users.noreply.github.com>
2024-03-27 10:34:20 +0000

    update skills model

--- src/model/skills.js
+++ src/model/skills.js
@@ -6,6 +6,8 @@ const skillSchema = new Schema({
   name: { type: String, required: true },
   proficiency: { type: String, required: true },
   experience: { type: String, required: true },
+  category: { type: String, enum: ['Soft Skills', 'Languages', 'Frameworks', 'Tools'], required: true },
+  icon: { type: String, required: true } // Assuming icons are stored as URLs
 });
 
 export const Skill = mongoose.model("Skill", skillSchema);
