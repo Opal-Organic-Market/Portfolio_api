d9e7ed7b77daedf61a4e229f04a40197bf61a032
teyvi <44750503+teyvi@users.noreply.github.com>
2024-03-27 19:44:14 +0000

    added category to the skills model

--- src/model/skills.js
+++ src/model/skills.js
@@ -6,6 +6,8 @@ const skillSchema = new Schema({
   name: { type: String, required: true },
   proficiency: { type: String, required: true },
   experience: { type: String, required: true },
+  category: { type: String, enum: ['Soft skills', 'Language', 'Tools', 'Framework'], required: true }
+ 
 });
 
 export const Skill = mongoose.model("Skill", skillSchema);
