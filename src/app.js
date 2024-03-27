b9786da800805e685540577236d3ae2e6c3b43d4
teyvi <44750503+teyvi@users.noreply.github.com>
2024-03-27 16:53:00 +0000

    swagger twerk

--- src/app.js
+++ src/app.js
@@ -3,10 +3,17 @@ import dotenv from "dotenv";
 import mongoose from "mongoose"
 import {router} from "./routes/allroutes.js"
 import cors from "cors";
-
+import { handleRequests, init, handleResponses } from "express-oas-generator";
+//const expressOasGenerator = require('express-oas-generator');
 
 dotenv.config();
+
+const modelNames = mongoose.modelNames();
+
 const app = express();
+handleResponses(app, {});
+
+dotenv.config();
 const PORT = process.env.PORT || 7020;
 
 //middlewares
@@ -28,5 +35,34 @@ try {
     
 }
 app.listen(PORT, () =>{
+    init(
+        app,
+        (spec) => {
+          spec.info = {
+            title: "Portfolio API Documentation",
+            description: "API Documentation for Portfolio website",
+          };
+          spec.host = "localhost:8000";
+          spec.schemes = ["http", "https"];
+    
+          return spec;
+        },
+        "./swagger.json",
+        60 * 1000,
+        "api-docs",
+        modelNames,
+        ["users", "skills"],
+        ["development"],
+        true
+      );
+    
+      console.log(
+        'Server Listening on  8080, Open http://localhost:8080/api-docs/'
+      );
+    });
+    
+    app.use(router);
+    
+    handleRequests();
     console.log(`express app is runnning at ${PORT}`)
-});
+
